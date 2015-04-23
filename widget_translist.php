c337e5f84cd895e4d6f49f0dbdc1a285a0e20082
bcfuchs <bcfuchs@gmail.com>
2015-04-23 11:25:05 +0000

    first cut list page

--- widget_translist.php
+++ widget_translist.php
@@ -3,7 +3,15 @@
 Template Name: Widget Translist
 */
 ?>
+<style>
+#kestrel-list img {
+width: 100px;
+
+}
+</style>
 <h2>Kestrel list</h2>
+<p>Just a list to see what we've got</p>
+
 <?php
 /** widget to display a list of kestrels with info when it is available 
 * To get the images: 
@@ -12,28 +20,50 @@ $query = 'select guid  from wp_posts inner join wp_postmeta on wp_posts.id=wp_po
 $res = $wpdb->get_results($query);
 
 * To get the readings: 
+
+select * from wp_kestrel_readings where image=$imageurl;
+* 
+* Better way to do this:
+* 1. get all the available kestrels
+* 2. List all the transciptions for that kestrel
+* 3. Put in list-groups inside a list-group
 *  
 */
-
-
 ?>
 <table id="kestrel-list" class="table">
-<th>
-<td>Name</td>
+<tr>
 <td>Image</td>
 <td>Location</td>
 <td>Wind Direction</td>
 <td>Wind Speed</td>
 <td>Temperature</td>
-</th>
-<tr>
-<td  class="kestrel-name"></td>
-<td  class="kestrel-image"></td>
-<td class="kestrel-location"></td>
-<td class="kestrel-wd"></td>
-<td class="kestrel-ws"></td>
-<td class="kestrel-temp"></td>
 </tr>
+<?php
+global $wpdb;
+$query = 'select * from wp_kestrel_readings';
+$res = $wpdb->get_results($query);
+// there will be more than two readings for a kestrel!
+
+foreach ($res as $img) {
+$lat = stripslashes($img->lat);
+$lon = stripslashes($img->lon);
+$out = <<<END
+
+<tr>
+
+<td  class="kestrel-image"><img src="$img->image"/></td>
+<td class="kestrel-location">$lat, $lon</td>
+<td class="kestrel-wd">$img->wind_direction</td>
+<td class="kestrel-ws">$img->wind_</td>
+<td class="kestrel-temp">$img->temp</td>
+</tr>
+END;
+echo $out;
+}
+
+
+?>
+
 </table>
 
 
